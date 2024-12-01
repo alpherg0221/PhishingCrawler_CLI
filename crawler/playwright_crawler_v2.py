b55d9532073665ea3145cdf07ff8e50ce1e3e0a6
alpherg0221 <alpherg0221@gmail.com>
2024-12-01 12:35:30 +0900

    Changed timeout

--- crawler/playwright_crawler_v2.py
+++ crawler/playwright_crawler_v2.py
@@ -39,6 +39,7 @@ async def playwright_main(mobile: str, url: str, target: str, gsb: str) -> str:
 
         # URLに移動
         await page.goto(url)
+        await page.wait_for_load_state()
         await page.wait_for_timeout(5000)
 
         # URLがパスのみの場合, .htmlを追加
@@ -58,6 +59,7 @@ async def playwright_main(mobile: str, url: str, target: str, gsb: str) -> str:
         page = await context.new_page()
         page.on("response", lambda res: save_resources(res, saved_dirname))  # リソース保存設定
         await page.goto(url)
+        await page.wait_for_load_state()
         await page.wait_for_timeout(5000)
 
         # スクショを保存
